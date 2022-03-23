f446b3f0dd17eb1f989cb2ee19c39b44cee30e2c
gusta <gusta@DESKTOP-BA7FTP4>
2022-03-23 16:02:41 -0300

    Adicionar arquivos de projeto.

---
+++ ListaContatos/Models/ContatoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ListaContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Observacao { get; set; }
+    }
+}
